aeeb26353198fea8614ac474d3282e04d4621504
Eric Storey <estorey@gmail.com>
2019-06-19 16:58:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function BoardMember(name, homeState, training){
   
   this.veto=function(){return "No, I must disagree"}
   this.approve=function(){return "You can do that!"}
-  this.doCharity=function(){return "You can do that!"}
+  this.doCharity=function(){return "I like to help people."}
   this.approve=function(){return "You can do that!"}
   this.approve=function(){return "You can do that!"}
 }
